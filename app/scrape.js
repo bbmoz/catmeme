ddc8020a2dab3c716b221df81f31a6fc417fb90f
bbmoz <blueberrymozart@gmail.com>
2016-02-07 11:02:18 -0600

    cache images and reset them every 6 hours

--- app/scrape.js
+++ app/scrape.js
@@ -4,6 +4,12 @@ module.exports = (function scrape() {
   const vo = require('vo');
   const request = Nightmare();
   let imageNamesStore;
+  let imageUrlStore = {};
+
+  setInterval(function () {
+    imageNamesStore = undefined;
+    imageUrlStore = {};
+  }, 21600); // every 6 hours
 
   return {
     previewImageNames: function (cb) {
@@ -11,7 +17,7 @@ module.exports = (function scrape() {
         if (imageNamesStore === undefined) {
           const imageNames = yield request
             .useragent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36')
-            .goto('https://www.google.com/search?q=cat+meme&tbm=isch&tbs=isz:lt,islt:svga')
+            .goto('https://www.google.com/search?q=cat+meme&tbm=isch&tbs=isz:m')
             .wait('img.rg_i')
             .evaluate(function () {
               const imageNames = [];
@@ -38,25 +44,30 @@ module.exports = (function scrape() {
 
     getActualImageSrc: function (imageName, cb) {
       vo(function* () {
-        const imageSrc = yield request
-          .goto(`https://www.google.com/search?q=cat+meme&tbm=isch&tbs=isz:lt,islt:svga#imgrc=${encodeURIComponent(imageName)}`)
-          .wait('#irc_cc img.irc_mi[src^="http"]')
-          .evaluate(function () {
-            const possibleImages = document.querySelectorAll('#irc_cc img.irc_mi[src^="http"]');
-            const length = possibleImages.length;
-            var correctImageIndex = 0;
-            for (var i = 1; i < length; i += 1) {
-              if (possibleImages[i].height > possibleImages[correctImageIndex].height) {
-                correctImageIndex = i;
+        if (imageUrlStore[imageName] !== undefined) {
+          return imageUrlStore[imageName];
+        } else {
+          const imageSrc = yield request
+            .goto(`https://www.google.com/search?q=cat+meme&tbm=isch&tbs=isz:m#imgrc=${encodeURIComponent(imageName)}`)
+            .wait('#irc_cc img.irc_mi[src^="http"]')
+            .evaluate(function () {
+              const possibleImages = document.querySelectorAll('#irc_cc img.irc_mi[src^="http"]');
+              const length = possibleImages.length;
+              var correctImageIndex = 0;
+              for (var i = 1; i < length; i += 1) {
+                if (possibleImages[i].height > possibleImages[correctImageIndex].height) {
+                  correctImageIndex = i;
+                }
               }
-            }
-            return possibleImages[correctImageIndex].src;
-          });
-        return imageSrc;
+              return possibleImages[correctImageIndex].src;
+            });
+          return imageSrc;
+        }
       })(function (error, imageSrc) {
         if (error) {
           cb(error);
         } else {
+          imageUrlStore[imageName] = imageSrc;
           cb(null, imageSrc)
         }
       });
